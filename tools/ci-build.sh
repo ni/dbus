7907e66bfcf7881b58c32de87ff505c084e695e2
Simon McVittie <smcv@collabora.com>
2023-10-20 16:32:02 +0100

    CI: Enable Containers1 in debug builds, explicitly disable in reduced build
    This ensures that both "enabled" and "disabled" are tested, regardless
    of whether it is enabled by default (currently it is not).
    
    Create /var/local/run/dbus/containers so that we can run the tests.
    
    Signed-off-by: Simon McVittie <smcv@collabora.com>

--- tools/ci-build.sh
+++ tools/ci-build.sh
@@ -358,6 +358,7 @@ case "$ci_buildsys" in
                         fi
 
                         set -- -Db_pie=true "$@"
+                        set -- -Dcontainers=true "$@"
                         set -- -Duser_session=true "$@"
                         ;;
                 esac
@@ -382,6 +383,7 @@ case "$ci_buildsys" in
                 set "$@" -Dlibaudit=disabled -Dvalgrind=disabled
                 # Disable optional features, some of which are on by
                 # default
+                set "$@" -Dcontainers=false
                 set "$@" -Dstats=false
                 set "$@" -Duser_session=false
                 shift
@@ -442,6 +444,12 @@ case "$ci_buildsys" in
 
         $meson_setup "$@" "$srcdir"
         meson compile -v
+
+        if [ "$(id -u)" = 0 ]; then
+            # In production, this would be /run/dbus/containers and would
+            # have been set up by the init script or tmpfiles.d
+            mkdir -p /var/local/run/dbus/containers
+        fi
 
         # This is too slow and verbose to keep enabled at the moment
         export DBUS_TEST_MALLOC_FAILURES=0
